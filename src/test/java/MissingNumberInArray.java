07416e386d3ffcac14f0a610594282b2dd95fbc6
Radhika <radhikagupta222@gmail.com>
2025-04-18 12:56:12 +0530

    second commit

---
+++ src/test/java/MissingNumberInArray.java
@@ -0,0 +1,12 @@
+public class MissingNumberInArray {
+
+    public static void main(String[] args) {
+      //  int[] arr={1,3,4,5,6};
+    }
+
+    public static void missing(int[] arr){
+        int[] arr1={1,3,4,5,6};
+
+
+    }
+}
